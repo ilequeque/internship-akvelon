3076f516aa3c11b83480782ea3ca95abb87fee75
ilequeque <ilyas.shakhabdinov@gmail.com>
2022-03-30 19:05:02 +0600

    final commit

--- week 1/Queue/Queue/Program.cs
+++ week 1/Queue/Queue/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace QueueTemp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            QueueSM<int> sm = new();
+            sm.Enqueue(1);
+            sm.Enqueue(2);
+            sm.Enqueue(3);
+            sm.Enqueue(4);
+            sm.Enqueue(5);
+            sm.Enqueue(6);
+            Console.WriteLine("{0} customers ready to be served.", sm.Size());
+            Console.WriteLine(sm.Peek());
+            Console.WriteLine("Customer number {0} was served.", sm.Dequeue());
+            Console.WriteLine("Customer number {0} was served.", sm.Dequeue());
+            Console.WriteLine("Customer number {0} was served.", sm.Dequeue());
+            Console.WriteLine("Customer number {0} was served.", sm.Dequeue());
+            Console.WriteLine("Customer number {0} was served.", sm.Dequeue());
+            Console.WriteLine("Customer number {0} was served.", sm.Dequeue());
+        }
+    }
+}
