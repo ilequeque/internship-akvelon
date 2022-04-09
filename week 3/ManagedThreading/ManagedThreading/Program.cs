3e2a8aa47c749ccedba9c9ba2149e72533ac9e98
ilequeque <ilyas.shakhabdinov@gmail.com>
2022-04-09 17:15:10 +0600

    first task start

---
+++ week 3/ManagedThreading/ManagedThreading/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Thread t = new(new ThreadStart(WriteY));
+            t.Start();
+
+            for (int i = 0; i < 10; i++) Console.Write("X");
+
+        }
+        public static void WriteY()
+        {
+            for (int i = 0; i < 10; i++) Console.Write("Y");
+        }
+    }
+}
