e2a8dc0bbc0b90d35e3f2755a95f3368f41b6931
ilequeque <ilyas.shakhabdinov@gmail.com>
2022-04-23 16:36:32 +0600

    ManagedThreadingTask2

---
+++ week 3/ManagedThreadingTask2/ManagedThreadingTask2/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+
+namespace Solution
+{
+    internal class Program
+    {
+        public const string UrlPath = "https://jsonplaceholder.typicode.com/photos";
+        public const string PhotosFilename = "photos.json";
+        public static void Main()
+        {
+            Console.WriteLine("Start");
+            CheckInfoJson();
+            GetPhotoUrls();
+        }
+        public static void Downloader(string path)
+        {
+            var client = new WebClient();  //have some problems with web client
+            try
+            {
+                client.DownloadFile(path, PhotosFilename);
+                client.Dispose();
+            }
+            catch
+            {
+                Console.WriteLine("error occured");
+            }
+        }
+
+        public static void CheckInfoJson()
+        {
+            if (File.Exists(PhotosFilename))
+            {
+                Console.WriteLine("File already exists");
+            }
+            else
+            {
+                Downloader(UrlPath);
+            }
+        }
+
+        public static List<string> GetPhotoUrls()
+        {
+            var strings = new List<String>();
+            JObject jsonObject = JObject.Parse(File.ReadAllText(PhotosFilename));
+
+            Console.WriteLine(jsonObject.Count);
+            return strings;
+        }
+
+        public static JObject ReadJsonFile(string path)
+        {
+            StreamReader file = File.OpenText(path);
+            JsonTextReader reader = new(file);
+            JObject jObject = (JObject)JToken.ReadFrom(reader);
+
+            return jObject;
+        }
+    }
+}
