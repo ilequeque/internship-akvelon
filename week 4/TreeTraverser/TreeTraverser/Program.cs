071bf40b7fc9958192c2e33b7b86708b5fd3b7da
ilequeque <ilyas.shakhabdinov@gmail.com>
2022-04-25 05:19:16 +0600

    Tree Traverser edited

--- week 4/TreeTraverser/TreeTraverser/Program.cs
+++ week 4/TreeTraverser/TreeTraverser/Program.cs
@@ -10,11 +10,11 @@ namespace TreeTraverser
             TestClass test2 = new TestClass(2, "Michael");
             TestClass test3 = new TestClass(3, "Dwight");
             
-            var temp = new Tree<TestClass>(test1);
-            temp.Left = new Tree<TestClass>(test2);
-            temp.Right = new Tree<TestClass>(test3);
-         
-            Action<TestClass> action = x => Console.WriteLine($"{x.Id}, {x.Name}");
+            var temp = new Tree<TestClass>(test1); // main tree
+            temp.Left = new Tree<TestClass>(test2); // left leaf of the tree
+            temp.Right = new Tree<TestClass>(test3); //right leaf of the tree
+
+            static void action(TestClass x) => Console.WriteLine($"{x.Id}, {x.Name}");
 
             Traverser.DoTree(temp, action);
         }
